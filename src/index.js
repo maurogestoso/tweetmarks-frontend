d8ebceb3222c68c1c63abf2fd84679dc59481620
Harriet Ryder <harriethryder@gmail.com>
2019-02-23 18:36:40 +0000

    Add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "@babel/polyfill";
+import "bootstrap/dist/css/bootstrap.css";
 import ReactDOM from "react-dom";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
